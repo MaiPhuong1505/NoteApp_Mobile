3e2fb4696b02d92886eaac01d3b7cb2314a986af
MaiPhuong1505 <maiphuongpham1505@gmail.com>
2022-05-26 18:56:58 +0700

    Initial commit, the UI

---
+++ app/src/main/java/com/example/noteapp_mobile/activities/AddNoteActivity.java
@@ -0,0 +1,16 @@
+package com.example.noteapp_mobile.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.noteapp_mobile.R;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+    }
+}
---
+++ app/src/main/java/com/example/noteapp_mobile/activities/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.noteapp_mobile.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.noteapp_mobile.R;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ChipNavigationBar chipNavigationBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chipNavigationBar = findViewById(R.id.bottom_navbar);
+        
+        bottomMenu();
+    }
+
+    private void bottomMenu() {
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+
+            }
+        });
+    }
+}
