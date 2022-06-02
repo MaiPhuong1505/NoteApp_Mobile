aad9bf8d4489490484db885ab503d0e85682b475
MaiPhuong1505 <maiphuongpham1505@gmail.com>
2022-06-03 00:01:52 +0700

    add DAO, DB and view function

---
+++ app/src/main/java/com/example/noteapp_mobile/adapters/MyNoteAdapter.java
@@ -0,0 +1,80 @@
+package com.example.noteapp_mobile.adapters;
+
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.noteapp_mobile.R;
+import com.example.noteapp_mobile.entities.MyNoteEntities;
+import com.makeramen.roundedimageview.RoundedImageView;
+
+import java.util.List;
+
+public class MyNoteAdapter extends RecyclerView.Adapter<MyNoteAdapter.ViewHolder> {
+
+    List<MyNoteEntities> noteEntitiesList;
+
+    public MyNoteAdapter(List<MyNoteEntities> noteEntitiesList) {
+        this.noteEntitiesList = noteEntitiesList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.setNote(noteEntitiesList.get(position));
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return noteEntitiesList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView txtItemTitle, txtItemNote, txtItemDate;
+        private LinearLayout linearLayout;
+        RoundedImageView roundedImageView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            txtItemTitle = itemView.findViewById(R.id.txt_item_title);
+            txtItemNote = itemView.findViewById(R.id.txt_item_note);
+            txtItemDate = itemView.findViewById(R.id.txt_item_date);
+            linearLayout = itemView.findViewById(R.id.layout_note);
+            roundedImageView = itemView.findViewById(R.id.image_note);
+
+        }
+
+        public void setNote(MyNoteEntities myNoteEntities) {
+            txtItemTitle.setText(myNoteEntities.getTitle());
+            txtItemNote.setText(myNoteEntities.getNoteText());
+            txtItemDate.setText(myNoteEntities.getDateTime());
+
+            GradientDrawable gradientDrawable = (GradientDrawable) linearLayout.getBackground();
+            if (myNoteEntities.getColor() != null){
+                gradientDrawable.setColor(Color.parseColor(myNoteEntities.getColor()));
+            }
+            else{
+                gradientDrawable.setColor(Color.parseColor("#FF937B"));
+            }
+        }
+    }
+}
